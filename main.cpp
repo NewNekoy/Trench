5b783f569d42a2db78fc3028809eab0fa9a33ee8
SoyNeko <hugo.haymann@epitech.eu>
2022-09-01 17:09:20 +0200

    add function to read double string vectors

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include "Core.h"
 
+void printDoubleVector(vector<vector<string>> test)
+{
+    for(int i = 0; i < test.size(); i++)
+    {
+        for (int j = 0; j < test[i].size(); j++)
+        {
+            cout << test[i][j];
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
 
     Core core;
 
-    vector<vector<string>> test = core.sql->GETTER("SELECT * FROM Account");
+    vector<vector<string>> test = core.sql->GETTER("SELECT * FROM Account WHERE username!='SoyNeko'");
 
-    cout << test[0][1] << endl;
+    printDoubleVector(test);
 
     return 0;
 }
