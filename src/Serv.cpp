ba55b8d764092a1b34beaa3a9e4762f5438490db
SoyNeko <hugo.haymann@epitech.eu>
2022-09-01 17:59:10 +0200

    Display other player in database

--- src/Serv.cpp
+++ src/Serv.cpp
@@ -14,7 +14,19 @@ Player::~Player()
 
 Serv::Serv(Core *core) : Scene(core)
 {
-    vector<vector<string>> OtherPlayers = core->sql->GETTER("SELECT * FROM Player WHERE name!='core._username'");
+    vector<vector<string>> OtherPlayers = core->sql->GETTER("SELECT * FROM Player WHERE name!='" + core->_username + "'");
+
+    Player temp;
+    temp.sprite.setSize(Vector2f(64, 64));
+    temp.sprite.setFillColor(Color::Red);
+
+    for(int i = 0; i < OtherPlayers.size(); i++)
+    {
+        temp.name = OtherPlayers[i][1];
+        temp.sprite.setPosition(Vector2f(stoi(OtherPlayers[i][2]), stoi(OtherPlayers[i][3])));
+        other.push_back(temp);
+    }
+
     me.name = core->_username;
 }
 
@@ -63,6 +75,13 @@ void Serv::use(Core *core)
 
     core->sfml->window.draw(me.sprite);
     core->sfml->printText(me.name, {me.sprite.getPosition().x - (me.sprite.getSize().x / 2), me.sprite.getPosition().y - 32}, 35, Color::Black);
+
+    for(int i = 0 ; i < other.size(); i++)
+    {
+        core->sfml->window.draw(other[i].sprite);
+        core->sfml->printText(other[i].name, {other[i].sprite.getPosition().x - (other[i].sprite.getSize().x / 2), other[i].sprite.getPosition().y - 32}, 35, Color::Black);
+
+    }
 
     core->sfml->window.display();
 }
